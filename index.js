cb8f00ebebd20e909e5956b2e925e81a6dbc9792
Greg Fisher <ichthyologicaldev@gmail.com>
2021-01-02 13:38:38 -0800

    add string, number, bigint, boolean questions

--- index.js
+++ index.js
@@ -1,2 +1,79 @@
-let something = "something";
-aosdijf;
+/*
+Welcome to the Data Types Practice Exercise! This exercise will let you practice
+with strings, numbers, booleans, and BigInt. There are many other data types in 
+JavaScript, but these 4 are the most common when describing information. Read through
+the rest of this comment for instructions, or jump right down to the requirements!
+
+Look through this file for requirements that start with //REQUIREMENT N:
+You will add your code on the line or lines that start with //TODO:
+
+EXAMPLE:
+//REQUIREMENT 1: Do Ichthyologists study fish, birds, or mammals?
+//TODO: On the following line, change null to your answer. Make sure that it's a string!
+let ichthyologistFocus = null;
+
+You would change the previous line so that it says: 
+let ichthyologistFocus = 'fish';
+
+You will occasionally see longer comments (like this one) that will include extra
+instruction, helpful hints, best practices, or other content. 
+
+IMPORTANT
+You should have the index.html file open in your browser as you work through this
+exercise. If you aren't sure what this means, you should watch the <INSERT LINK HERE> 
+video before continuing.
+
+I hope that you get hooked on coding!
+*/
+
+//REQUIREMENT 1: Do Ichthyologists study fish, birds, or mammals?
+//TODO: On the following line, change null to your answer. Make sure that it's a string!
+let ichthyologistFocus = 'fish';
+
+//REQUIREMENT 2: What is the state fish of Hawaii? (The one that's fun to say!)
+//TODO: On the following line, change null to your answer. Make sure that it's a string!
+let hawaiianStateFish = 'humuhumu';
+
+//REQUIREMENT 3: How many oceans are there?
+//TODO: On the following line, change null to your answer. Make sure that it's a number!
+let numberOfOceans = null;
+
+//REQUIREMENT 4: What is your name?
+//TODO: On the following line, change null to your answer. Make sure that it's a string!
+let name = null;
+
+//REQUIREMENT 5: True or false, you are having fun wielding your knowledge of data types!
+//TODO: On the following line, change null to your answer. Make sure that it's a boolean!
+let havingFun = null;
+
+//REQUIREMENT 6: What is the longest river in the world?
+//TODO: On the following line, change null to your answer. Make sure that it's a string!
+let longestRiver = null;
+
+//REQUIREMENT 7: How many kilometers long is the Congo River?
+//TODO: On the following line, change null to your answer. Make sure that it's a number!
+let congoLength = null;
+
+//REQUIREMENT 8: True or false, you have caught a fish.
+//TODO: On the following line, change null to your answer. Make sure that it's a boolean!
+let haveCaughtFish = null;
+
+//REQUIREMENT 9: True or false, JavaScript is rarely used in large tech companies.
+//TODO: On the following line, change null to your answer. Make sure that it's a boolean!
+let javaScriptIsRarelyUsed = null;
+
+//REQUIREMENT 10: True or false, you can use the BigInt with decimals.
+//TODO: On the following line, change null to your answer. Make sure that it's a boolean!
+let bigIntWorksForDecimals = null;
+
+//REQUIREMENT 11: How would you store the answer to (9007199254740992 + 1) as a BigInt?
+//TODO: On the following line, change null to your answer. Make sure that it's a BigInt!
+let largeNumber = null;
+
+//REQUIREMENT 12: How many salmon species are native to North America?
+//TODO: On the following line, change null to your answer. Make sure that it's a number!
+let totalNaSalmonSpecies = null;
+
+//REQUIREMENT 13: True or false, the previous requirements covered all possible JavaScript data types.
+//TODO: On the following line, change null to your answer. Make sure that it's a boolean!
+let coveredAllPossibleDataTypes = null;
--- validation.js
+++ validation.js
@@ -1,3 +1,146 @@
-const add = (a,b) => a+b;
+/*
+WARNING!!!!!
+This file checks your work. If you make changes to this file, then the tests might not work.
+*/
 
-const getSomething = () => something;
+/*
+TEMPLATE GUIDE
+Remove this comment after you've made changes to a new exercise.
+
+A recommended pattern is to create functions for each requirement.  
+*/
+
+/**
+ * Writes a result message to the DOM
+ * @param {number} questionIndex - DOM index for this question
+ * @param {string} result - message to be written
+ * @param {boolean} success - whether the test passed
+ */
+const writeResult = (questionIndex, result, success) => {
+    const resultElement = document.querySelectorAll('.problem-container > .question')[questionIndex].querySelector('.result');
+    const textNode = document.createTextNode(result);
+    resultElement.appendChild(textNode);
+
+    resultElement.classList.toggle('pass', success);
+}
+
+/**
+ * Main function. Runs validation on all provided questions.
+ * @param {Object[]} questions - Array of questions
+ */
+const runValidation = (questions) => {
+    questions.forEach((question, index) => {
+        question.validate();
+
+        writeResult(index, question.message, question.passed);
+    });
+}
+
+/**
+ * Creates a question object.
+ * @param {function} validator - function that returns a boolean based on this question passing
+ * @param {string} success - message if result passes validation
+ * @param {string} failure  - message if result fails validation
+ * 
+ * @returns {Object} - question object
+ */
+const question = (validator) => {
+    return {
+        validate: function () {
+            try {
+                const { passed, message } = validator();
+                this.message = message;
+                this.passed = passed;
+            }
+            catch (e) {
+                if (e.name === 'ReferenceError') {
+                    this.message = 'Your code has an error at or before this question. Open up your console to see more information.'
+                    this.passed = false;
+                }
+            }
+        }
+    }
+}
+
+/**
+ * 
+ * @param {boolean} passed - Whether the requirement was met
+ * @param {string} message - message to display
+ * @returns {object} - Result Object
+ */
+const resultObject = (passed, message) => ({ passed, message })
+
+/*
+***********************************************************************************
+*************************************IMPORTANT*************************************
+***********************************************************************************
+Everything below can be safely removed from the template. You MIGHT want to keep the 
+getTypeMessage function, depending on the problems that you have in mind. Perhaps 
+additional helper methods will be added later. This template will be updated accordingly.
+There is a short example of what validation functions might look like, as well as an
+example of triggering validation.
+***********************************************************************************
+***********************************************************************************
+*/
+
+
+/**
+ * A guard clause that returns a message depending on the success or issue.
+ * @param {*} variable - The variable being changed for this requirement
+ * @param {string} type - Data type that this variable should be
+ * @returns {string} - "success" if the type matches, otherwise a message to be displayed to the user
+ */
+const getTypeMessage = (variable, type) => {
+    let message = 'success';
+    if (variable === null) {
+        message = 'Did you make a change yet? If so, make sure you saved your file and refreshed this page!'
+    }
+    else if (typeof variable !== type) {
+        message = `We were expecting a string, but you entered a ${typeof variable}.`;
+    }
+    return message;
+}
+
+const validateIchthyologistFocus = () => {
+    let message = getTypeMessage(ichthyologistFocus, 'string');
+    if (message === 'success') {
+        if(ichthyologistFocus == 'fish') {
+            message = 'Correct! Ichthyologists study fish';
+        }
+        else {
+            message = `Not quite, Ichthyologists don't study ${ichthyologistFocus}. Do a quick google search and try again!`;
+        }
+        return resultObject(true, message);
+    }
+    else {
+        return resultObject(false, message);
+    }
+}
+
+const validateHawaiianStateFish = () => {
+    let message = getTypeMessage(hawaiianStateFish, 'string');
+    if (message === 'success') {
+        let fish = hawaiianStateFish.toLowerCase().replace("'",).replace('ā', 'a');
+        switch (fish) {
+            case 'humuhumunukunukuapuaa':
+            case 'humuhumu':
+                return resultObject(true, "Ae! The Humuhumunukunukuapua'a is Hawaii's state fish. Try saying that 10 times.");
+            case 'reef triggerfish':
+                return resultObject(false, "You're technically right, but Reef Triggerfish isn't as fun to say as its other name.");
+            default:
+                return resultObject(false, `Mahalo, but no, we were looking for humuhumunukunukuapua'a, not ${hawaiianStateFish}.`)
+
+        }
+    }
+    else {
+        return resultObject(false, message);
+    }
+}
+
+//NOTE: The order of questions in this array must match the order of requirements on the DOM and in the index.js file.
+const questions = [
+    question(validateIchthyologistFocus),
+    question(validateHawaiianStateFish)
+]
+
+runValidation(questions);
