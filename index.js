714379d5b75af686cafdda65ffa1915bce88d828
Ichthyological Dev <ichthyologicaldev@gmail.com>
2021-01-16 13:34:11 -0800

    solution complete

--- index.js
+++ index.js
@@ -28,59 +28,59 @@ I hope that you get hooked on coding!
 
 //REQUIREMENT 1: Do Ichthyologists study fish, birds, or mammals?
 //TODO: On the following line, change null to your answer. Make sure that it's a string!
-let ichthyologistFocus = null;
+let ichthyologistFocus = 'fish';
 
 //REQUIREMENT 2: What is your name?
 //TODO: On the following line, change null to your answer. Make sure that it's a string!
-let developerName = null;
+let developerName = 'nunya';
 
 //REQUIREMENT 3: How many oceans are there?
 //TODO: On the following line, change null to your answer. Make sure that it's a number!
-let numberOfOceans = null;
+let numberOfOceans = 5;
 
 //REQUIREMENT 4: True or false, you are having fun wielding your knowledge of JavaScript data types!
 //TODO: On the following line, change null to your answer. Make sure that it's a boolean!
-let havingFun = null;
+let havingFun = true;
 
 //REQUIREMENT 5: What is the state fish of Hawaii? (The one that's fun to say!)
 //TODO: On the following line, change null to your answer. Make sure that it's a string!
-let hawaiianStateFish = null;
+let hawaiianStateFish = 'humuhumu';
 
 //REQUIREMENT 6: How many kilometers long is the Congo River?
 //TODO: On the following line, change null to your answer. Make sure that it's a number!
-let congoLength = null;
+let congoLength = 4370;
 
 //REQUIREMENT 7: True or false, you have caught a fish.
 //TODO: On the following line, change null to your answer. Make sure that it's a boolean!
-let haveCaughtFish = null;
+let haveCaughtFish = true;
 
 //REQUIREMENT 8: The largest regular number that JavaScript can reliably handle is 9007199254740991. Make a BigInt that's larger!
 //TODO: On the following line, change null to your answer. Make sure that it's a BigInt!
-let bigInteger = null;
+let bigInteger = 9007199254740992n;
 
 //REQUIREMENT 9: True or false, JavaScript is rarely used in large tech companies.
 //TODO: On the following line, change null to your answer. Make sure that it's a boolean!
-let javaScriptIsRarelyUsed = null;
+let javaScriptIsRarelyUsed = false;
 
 //REQUIREMENT 10: True or false, you can use the BigInt data type with decimals.
 //TODO: On the following line, change null to your answer. Make sure that it's a boolean!
-let bigIntWorksForDecimals = null;
+let bigIntWorksForDecimals = false;
 
 //REQUIREMENT 11: How would you store the answer to (9007199254740992 + 1) as a BigInt?
 //TODO: On the following line, change null to your answer. Make sure that it's a BigInt!
-let largeNumber = null;
+let largeNumber = 9007199254740993n;
 
 //REQUIREMENT 12: How many species of salmon are native to North America?
 //TODO: On the following line, change null to your answer. Make sure that it's a number!
-let totalNaSalmonSpecies = null;
+let totalNaSalmonSpecies = 6;
 
 //REQUIREMENT 13: What is the longest river in the world?
 //TODO: On the following line, change null to your answer. Make sure that it's a string!
-let longestRiver = null;
+let longestRiver = 'Nile';
 
 //REQUIREMENT 14: True or false, the previous requirements covered all possible JavaScript data types.
 //TODO: On the following line, change null to your answer. Make sure that it's a boolean!
-let seenAllTypes = null;
+let seenAllTypes = false;
 
 /*
 You've reached the end of this exercise! If you need more practice with data types, you're in luck.
